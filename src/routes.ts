8cd4bc264a514c7fd94d9c0487904ded7118dc6e
mansurdewan <mansurdewan0123@gmail.com>
2025-02-02 20:59:41 +0600

    Add deployment file

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { bookRoutes } from "./books/bookRoutes";
 import { memberRoutes } from "./members/memberRoutes";
 import { borrowRoutes } from "./borrow/borrowRoutes";
@@ -8,3 +8,8 @@ export const router = express.Router();
 router.use("/api/books", bookRoutes);
 router.use("/api/members", memberRoutes);
 router.use("/api", borrowRoutes);
+router.get("/", (res: Response, req: Request) => {
+  res
+    .status(200)
+    .json({ success: true, message: "Well come to library management server" });
+});
